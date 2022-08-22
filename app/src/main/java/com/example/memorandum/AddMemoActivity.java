ed79c39bc021823786188e5d78310607ab161660
LucaAnto01 <luca.antognarelli@gmail.com>
2022-08-22 23:25:22 +0200

    Creation Add Memo Activity

---
+++ app/src/main/java/com/example/memorandum/AddMemoActivity.java
@@ -0,0 +1,16 @@
+package com.example.memorandum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMemoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_memo);
+    }
+}
