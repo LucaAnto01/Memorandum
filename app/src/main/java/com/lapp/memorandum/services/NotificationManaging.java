bae39fbeda12d38da96af620ed705cc1c2eedd5c
LucaAnto01 <luca.antognarelli@gmail.com>
2022-09-04 19:38:29 +0200

    Creation of Notification Managing system

---
+++ app/src/main/java/com/lapp/memorandum/services/NotificationManaging.java
@@ -0,0 +1,87 @@
+package com.lapp.memorandum.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.lapp.memorandum.R;
+
+import java.util.Random;
+
+public class NotificationManaging extends ContextWrapper
+{
+    /*Attributes*/
+    private Context context;
+    private final String channelName = "Memorandum notification channel";
+    private final String channelId = "com.lapp.memorandum.services: " + channelName;
+
+    /**
+     * Constructor method
+     * @param base
+     */
+    public NotificationManaging(Context base)
+    {
+        super(base);
+        this.context = base;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) //Check version
+            createChannel();
+
+    }
+
+    /**
+     * Method to create a NotificationChannel
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createChannel()
+    {
+        NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+        notificationChannel.enableLights(true);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setDescription("Memorandum notification");
+        notificationChannel.setLightColor(Color.GREEN);
+        notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(notificationChannel);
+    }
+
+    /**
+     * Method to create new notification
+     * @param title
+     * @param description
+     * @param id --> corresponds to Memo id
+     * @param activityName
+     */
+    public void createHighPriorityNotification(String title, String description, int id, Class activityName)
+    {
+        //This permit to open app (in a activityName interface) when we tap the notification
+        Intent intent = new Intent(this, activityName);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification =(new NotificationCompat.Builder(this, channelId) //Create a notification
+                //.setContentTitle(title)
+                //.setContentText(body)
+                .setSmallIcon(R.drawable.memo_map_icon)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle().setSummaryText("Memorandum").setBigContentTitle(title).bigText(description))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true) //Delete when click on the notification
+                .build());
+
+        //Send the notification
+        //--> use id to not repeat the notification
+        NotificationManagerCompat.from(this).notify(id, notification);
+
+    }
+}
--- app/src/main/java/com/lapp/memorandum/utils/MemoAppData.java
+++ app/src/main/java/com/lapp/memorandum/utils/MemoAppData.java
@@ -4,18 +4,21 @@ import android.Manifest;
 import android.content.Context;
 
 import com.lapp.memorandum.models.Location;
+import com.lapp.memorandum.services.NotificationManaging;
 
 public class MemoAppData
 {
-    private static String apiKey = "AIzaSyDoi0uX-_x-iFBQtlSZpeFnTRWAybdbM_E";
-    private static final String[] permissions = {
+    private static String apiKey = "AIzaSyDoi0uX-_x-iFBQtlSZpeFnTRWAybdbM_E";  //Api key
+    private static final String[] permissions = {                              //All necessary permissions
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_BACKGROUND_LOCATION,
             Manifest.permission.INTERNET};
-    private static PermissionManager permissionManager = null; //Default is null
-    private static Location userLocation = null;
+    private static PermissionManager permissionManager = null;                 //Permission manager
+    private static Location userLocation = null;                               //User location
+    private static NotificationManaging notificationManaging;                  //Notification manager
 
+    /*Getters & Setters*/
     public static String getApiKey()
     {
         return apiKey;
@@ -41,4 +44,8 @@ public class MemoAppData
     public static Location getUserLocation() { return userLocation; }
 
     public static void setUserLocation(Location userLocation) { MemoAppData.userLocation = userLocation; }
+
+    public static NotificationManaging getNotificationManaging() { return notificationManaging; }
+
+    public static void setNotificationManaging(NotificationManaging notificationManaging) { MemoAppData.notificationManaging = notificationManaging; }
 }
